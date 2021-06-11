21cf70b552d5b807e15ef1dbcc784a6eeb519503
XY351GTHO <84167420+XY351GTHO@users.noreply.github.com>
2021-06-11 13:36:27 +1200

    Add files via upload

---
+++ StartExchangeSession.ps1
@@ -0,0 +1,7 @@
+﻿# Run this to connect to Exchange Server
+$Cred = Get-Credential
+$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri http://exchange.examplecompany.net/PowerShell/ -Authentication Kerberos -Credential $Cred
+Import-PSSession $Session
+
+# When finished run this to Disconnect Exchange Session
+Remove-PSSession $Session
